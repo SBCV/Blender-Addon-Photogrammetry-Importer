a7bcaafaa565bf9c52c4d8ffa055858196471d57
sebastian <sebastian.bullinger@iosb.fraunhofer.de>
2018-03-16 14:30:46 +0100

    Fixed bug in texture usage while importing multiple objects

--- nvm_import_export/import_nvm_op.py
+++ nvm_import_export/import_nvm_op.py
@@ -93,71 +93,67 @@ def add_points_as_mesh(points, add_points_as_particle_system, mesh_type, point_e
 
         if add_points_as_particle_system:
             
-            if not viz_mesh.data.materials:
-                material_name = "PointCloudMaterial"
-                material = bpy.data.materials.get(material_name)
-                if material is None:
-                    material = bpy.data.materials.new(name=material_name)
-                viz_mesh.data.materials.append(material)
+            material_name = "PointCloudMaterial"
+            material = bpy.data.materials.new(name=material_name)
+            viz_mesh.data.materials.append(material)
+            
+            # enable cycles, otherwise the material has no nodes
+            bpy.context.scene.render.engine = 'CYCLES'
+            material.use_nodes = True
+            node_tree = material.node_tree
+            if 'Material Output' in node_tree.nodes:    # is created by default
+                material_output_node = node_tree.nodes['Material Output']
+            else:
+                material_output_node = node_tree.nodes.new('ShaderNodeOutputMaterial')
+            if 'Diffuse BSDF' in node_tree.nodes:       # is created by default
+                diffuse_node = node_tree.nodes['Diffuse BSDF']
+            else:
+                diffuse_node = node_tree.nodes.new("ShaderNodeBsdfDiffuse")
+            node_tree.links.new(diffuse_node.outputs['BSDF'], material_output_node.inputs['Surface'])
+            
+            if 'Image Texture' in node_tree.nodes:
+                image_texture_node = node_tree.nodes['Image Texture']
+            else:
+                image_texture_node = node_tree.nodes.new("ShaderNodeTexImage")
+            node_tree.links.new(image_texture_node.outputs['Color'], diffuse_node.inputs['Color'])
+            
+            vis_image_height = 1
+            
+            # To view the texture we set the height of the texture to vis_image_height 
+            image = bpy.data.images.new('ParticleColor', len(point_world_coordinates), vis_image_height)
+            
+            print('len(points): ' + str(len(points)))
+            print(type(image.pixels))
+            
+            # working on a copy of the pixels results in a MASSIVE performance speed
+            local_pixels = list(image.pixels[:])
+            print(type(local_pixels))
+            
+            num_points = len(points)
+            
+            for j in range(vis_image_height):
+                for point_index, point in enumerate(points):
+                    column_offset = point_index * 4     # (R,G,B,A)
+                    row_offset = j * 4 * num_points
+                    color = point.color 
+                    # Order is R,G,B, opacity
+                    local_pixels[row_offset + column_offset] = color[0] / 255.0
+                    local_pixels[row_offset + column_offset + 1] = color[1] / 255.0
+                    local_pixels[row_offset + column_offset + 2] = color[2] / 255.0
+                    # opacity (0 = transparent, 1 = opaque)
+                    #local_pixels[row_offset + column_offset + 3] = 1.0    # already set by default   
                 
-                # enable cycles, otherwise the material has no nodes
-                bpy.context.scene.render.engine = 'CYCLES'
-                material = bpy.data.materials['PointCloudMaterial']
-                material.use_nodes = True
-                node_tree = material.node_tree
-                if 'Material Output' in node_tree.nodes:
-                    material_output_node = node_tree.nodes['Material Output']
-                else:
-                    material_output_node = node_tree.nodes.new('ShaderNodeOutputMaterial')
-                if 'Diffuse BSDF' in node_tree.nodes:
-                    diffuse_node = node_tree.nodes['Diffuse BSDF']
-                else:
-                    diffuse_node = node_tree.nodes.new("ShaderNodeBsdfDiffuse")
-                node_tree.links.new(diffuse_node.outputs['BSDF'], material_output_node.inputs['Surface'])
-                
-                if 'Image Texture' in node_tree.nodes:
-                    image_texture_node = node_tree.nodes['Image Texture']
-                else:
-                    image_texture_node = node_tree.nodes.new("ShaderNodeTexImage")
-                node_tree.links.new(image_texture_node.outputs['Color'], diffuse_node.inputs['Color'])
-                
-                vis_image_height = 1
-                
-                # To view the texture we set the height of the texture to vis_image_height 
-                image = bpy.data.images.new('ParticleColor', len(point_world_coordinates), vis_image_height)
-                
-                print('len(points): ' + str(len(points)))
-                print(type(image.pixels))
-                
-                # working on a copy of the pixels results in a MASSIVE performance speed
-                local_pixels = list(image.pixels[:])
-                print(type(local_pixels))
-                
-                num_points = len(points)
-                
-                for j in range(vis_image_height):
-                    for point_index, point in enumerate(points):
-                        column_offset = point_index * 4     # (R,G,B,A)
-                        row_offset = j * 4 * num_points
-                        color = point.color 
-                        # Order is R,G,B, opacity
-                        local_pixels[row_offset + column_offset] = color[0] / 255.0
-                        local_pixels[row_offset + column_offset + 1] = color[1] / 255.0
-                        local_pixels[row_offset + column_offset + 2] = color[2] / 255.0
-                        # opacity (0 = transparent, 1 = opaque)
-                        #local_pixels[row_offset + column_offset + 3] = 1.0    # already set by default   
-                    
-                image.pixels = local_pixels[:]  
-                
-                image_texture_node.image = image
-                particle_info_node = node_tree.nodes.new('ShaderNodeParticleInfo')
-                divide_node = node_tree.nodes.new('ShaderNodeMath')
-                divide_node.operation = 'DIVIDE'
-                node_tree.links.new(particle_info_node.outputs['Index'], divide_node.inputs[0])
-                divide_node.inputs[1].default_value = num_points
-                shader_node_combine = node_tree.nodes.new('ShaderNodeCombineXYZ')
-                node_tree.links.new(divide_node.outputs['Value'], shader_node_combine.inputs['X'])
-                node_tree.links.new(shader_node_combine.outputs['Vector'], image_texture_node.inputs['Vector'])
+            image.pixels = local_pixels[:]  
+            
+            image_texture_node.image = image
+            particle_info_node = node_tree.nodes.new('ShaderNodeParticleInfo')
+            divide_node = node_tree.nodes.new('ShaderNodeMath')
+            divide_node.operation = 'DIVIDE'
+            node_tree.links.new(particle_info_node.outputs['Index'], divide_node.inputs[0])
+            divide_node.inputs[1].default_value = num_points
+            shader_node_combine = node_tree.nodes.new('ShaderNodeCombineXYZ')
+            node_tree.links.new(divide_node.outputs['Value'], shader_node_combine.inputs['X'])
+            node_tree.links.new(shader_node_combine.outputs['Vector'], image_texture_node.inputs['Vector'])
             
             if len(meshobj.particle_systems) == 0:
                 meshobj.modifiers.new("particle sys", type='PARTICLE_SYSTEM')
