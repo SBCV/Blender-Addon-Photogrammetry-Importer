12b46369d8b865e8f53188bd876199bfe9ca2d97
sebastian <sebastian.bullinger@iosb.fraunhofer.de>
2020-11-02 17:46:16 +0100

    Added vertex colors to mesh shader nodes

--- photogrammetry_importer/properties/mesh_import_properties.py
+++ photogrammetry_importer/properties/mesh_import_properties.py
@@ -2,6 +2,10 @@ import os
 import bpy
 from bpy.props import BoolProperty
 from photogrammetry_importer.utility.blender_logging_utility import log_report
+from photogrammetry_importer.utility.blender_mesh_utility import (
+    add_color_emission_to_material,
+    add_mesh_vertex_color_material,
+)
 
 
 class MeshImportProperties:
@@ -16,9 +20,18 @@ class MeshImportProperties:
         default=False,
     )
 
+    add_mesh_color_emission: BoolProperty(
+        name="Add Color Emission of Mesh",
+        description="Enabling color emission improves the visibility of the"
+        + "mesh colors",
+        default=True,
+    )
+
     def draw_mesh_options(self, layout):
         mesh_box = layout.box()
         mesh_box.prop(self, "import_mesh")
+        if self.import_mesh:
+            mesh_box.prop(self, "add_mesh_color_emission")
 
     def import_photogrammetry_mesh(self, mesh_fp, reconstruction_collection):
         if self.import_mesh and mesh_fp is not None:
@@ -31,6 +44,7 @@ class MeshImportProperties:
                     filepath=mesh_fp, axis_forward="Y", axis_up="Z"
                 )
             elif os.path.splitext(mesh_fp)[1].lower() == ".ply":
+                # https://docs.blender.org/api/current/bpy.ops.import_mesh.html
                 bpy.ops.import_mesh.ply(filepath=mesh_fp)
             else:
                 assert False
@@ -38,4 +52,19 @@ class MeshImportProperties:
             imported_object = bpy.context.selected_objects[-1]
             reconstruction_collection.objects.link(imported_object)
             previous_collection.objects.unlink(imported_object)
+
+            mesh_has_texture = len(imported_object.data.materials) > 0
+            mesh_has_vertex_color = "Col" in imported_object.data.vertex_colors
+
+            if mesh_has_texture:
+                if self.add_mesh_color_emission:
+                    add_color_emission_to_material(imported_object)
+            else:
+                if mesh_has_vertex_color:
+                    add_mesh_vertex_color_material(
+                        imported_object,
+                        "VertexColorMaterial",
+                        add_mesh_color_emission=self.add_mesh_color_emission,
+                    )
+
             log_report("INFO", "Importing mesh: Done", self)
---
+++ photogrammetry_importer/utility/blender_mesh_utility.py
@@ -0,0 +1,58 @@
+import bpy
+from photogrammetry_importer.utility.blender_logging_utility import log_report
+
+
+def add_color_emission_to_material(mesh_obj):
+    for material in mesh_obj.data.materials:
+        node_tree = material.node_tree
+        if "Principled BSDF" not in node_tree.nodes:  # is created by default
+            break
+        principled_bsdf_node = node_tree.nodes["Principled BSDF"]
+        link = principled_bsdf_node.inputs["Base Color"].links[0]
+        input_socket = link.from_socket
+
+        node_tree.links.new(
+            input_socket,
+            principled_bsdf_node.inputs["Emission"],
+        )
+
+
+def add_mesh_vertex_color_material(
+    mesh_obj,
+    mesh_material_name,
+    add_mesh_color_emission,
+):
+    material = bpy.data.materials.new(name=mesh_material_name)
+    mesh_obj.data.materials.append(material)
+
+    # Enable cycles - otherwise the material has no nodes
+    bpy.context.scene.render.engine = "CYCLES"
+    material.use_nodes = True
+    node_tree = material.node_tree
+
+    if "Material Output" in node_tree.nodes:  # is created by default
+        material_output_node = node_tree.nodes["Material Output"]
+    else:
+        material_output_node = node_tree.nodes.new("ShaderNodeOutputMaterial")
+
+    if "Principled BSDF" in node_tree.nodes:  # is created by default
+        principled_bsdf_node = node_tree.nodes["Principled BSDF"]
+    else:
+        principled_bsdf_node = node_tree.nodes.new("ShaderNodeBsdfPrincipled")
+    node_tree.links.new(
+        principled_bsdf_node.outputs["BSDF"],
+        material_output_node.inputs["Surface"],
+    )
+
+    attribute_node = node_tree.nodes.new("ShaderNodeAttribute")
+    # Use the vertex colors (stored in "Col") as colors
+    attribute_node.attribute_name = "Col"
+    node_tree.links.new(
+        attribute_node.outputs["Color"],
+        principled_bsdf_node.inputs["Base Color"],
+    )
+    if add_mesh_color_emission:
+        node_tree.links.new(
+            attribute_node.outputs["Color"],
+            principled_bsdf_node.inputs["Emission"],
+        )
