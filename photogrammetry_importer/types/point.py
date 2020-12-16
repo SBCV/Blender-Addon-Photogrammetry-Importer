becfc2dced4fb5bc150ecad04cc972fd209aada1
Sebastian Bullinger <sebastian.bullinger@iosb.fraunhofer.de>
2020-12-16 18:45:49 +0100

    Added docstrings to the point class

--- photogrammetry_importer/types/point.py
+++ photogrammetry_importer/types/point.py
@@ -2,13 +2,15 @@ from collections import namedtuple
 
 
 class Point(namedtuple("Point", ["coord", "color", "id", "scalars"])):
-    """
-    This class represents a three-dimensional point with the following information:
-    3D coordinate, color, point id and a list of scalars
+    """This class represents a three-dimensional point.
+
+    A point contains the following attributes: 3D coordinate, color, point id
+    and a list of scalars.
     """
 
     @staticmethod
     def split_points(points):
+        """Split points into positions and colors."""
         positions = []
         colors = []
         for point in points:
