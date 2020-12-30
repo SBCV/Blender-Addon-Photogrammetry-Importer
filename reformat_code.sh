0383f0c3c8127ac76078659945c5a8799246727e
Sebastian Bullinger <sebastian.bullinger@iosb.fraunhofer.de>
2020-12-30 12:46:57 +0100

    ensure correct directory in reformat_code.sh

--- reformat_code.sh
+++ reformat_code.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+# Go to the directory where the script is located
+cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd
 black --line-length 79 --exclude photogrammetry_importer/ext photogrammetry_importer
 black --line-length 79 doc/sphinx/source/conf.py
