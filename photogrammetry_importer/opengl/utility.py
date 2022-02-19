4e2e5fed08c40457fd23a00c73958c01327072db
Sebastian <sebastian.bullinger@iosb.fraunhofer.de>
2022-02-19 23:21:24 +0100

    Restore depth values in gpu.state to avoid potential freeze

--- photogrammetry_importer/opengl/utility.py
+++ photogrammetry_importer/opengl/utility.py
@@ -136,6 +136,9 @@ def render_opengl_image(image_name, cam, coords, colors, point_size):
     with offscreen.bind():
 
         gpu.state.point_size_set(point_size)
+        
+        previous_depth_mask_value = gpu.state.depth_mask_get()
+        previous_depth_test_value = gpu.state.depth_test_get()
         gpu.state.depth_mask_set(True)
         gpu.state.depth_test_set('LESS_EQUAL')
 
@@ -157,6 +160,9 @@ def render_opengl_image(image_name, cam, coords, colors, point_size):
             shader, "POINTS", {"pos": coords, "color": colors}
         )
         batch.draw(shader)
+        
+        gpu.state.depth_mask_set(previous_depth_mask_value)
+        gpu.state.depth_test_set(previous_depth_test_value)
 
         buffer = frame_buffer.read_color(0, 0, width, height, 4, 0, 'UBYTE')
 
