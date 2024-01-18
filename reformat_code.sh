882246a1be853c0461fd29a86827a2380ee67b66
Sebastian Bullinger <sebastian.bullinger@iosb.fraunhofer.de>
2024-01-18 13:29:24 +0100

    Update reformat_code.sh

--- reformat_code.sh
+++ reformat_code.sh
@@ -2,4 +2,5 @@
 # Go to the directory where the script is located
 cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd
 black --line-length 79 --exclude photogrammetry_importer/ext photogrammetry_importer
+black --line-length 79 example_view_synthesis_scripts
 black --line-length 79 doc/sphinx/source/conf.py
