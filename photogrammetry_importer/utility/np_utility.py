91632c5e45852882b7e29b0732dff6329c2e5769
Sebastian Bullinger <sebastian.bullinger@iosb.fraunhofer.de>
2024-01-22 11:36:33 +0100

    Adjust function name

--- photogrammetry_importer/utility/np_utility.py
+++ photogrammetry_importer/utility/np_utility.py
@@ -1,8 +1,8 @@
 import numpy as np
 
 
-def is_rotation_mat_valid(some_mat):
-    """ Test if a matrix is a rotation matrix."""
+def is_rotation_mat(some_mat):
+    """Test if a matrix is a rotation matrix."""
     # (i.e. det = -1 or det = 1)
     det = np.linalg.det(some_mat)
     res = np.isclose(det, 1) or np.isclose(det, -1)
