d18791d1252f6f05ff7f694b2c20fa68829e267f
Sebastian Bullinger <sebastian.bullinger@iosb.fraunhofer.de>
2024-01-22 11:37:51 +0100

    Add inversion function for transformations

--- photogrammetry_importer/utility/np_utility.py
+++ photogrammetry_importer/utility/np_utility.py
@@ -7,3 +7,13 @@ def is_rotation_mat(some_mat):
     det = np.linalg.det(some_mat)
     res = np.isclose(det, 1) or np.isclose(det, -1)
     return res
+
+
+def invert_transformation_matrix(mat):
+    """Invert transformation matrix without numerical errors."""
+    assert is_rotation_mat(mat)
+    # Analogue to set_camera_translation_vector_after_rotation()
+    mat_copy = np.array(mat).copy()
+    mat_copy[0:3, 3] = -np.dot(mat[0:3, 0:3].T, mat_copy[0:3, 3])
+    mat_copy[0:3, 0:3] = mat[0:3, 0:3].T
+    return mat_copy
