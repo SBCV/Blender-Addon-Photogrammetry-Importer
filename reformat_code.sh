3728d89572a8c10656381f0b2d8cf79d7fab1abd
Sebastian <sebastian.bullinger@iosb.fraunhofer.de>
2024-03-19 18:53:18 +0100

    Add black required version

--- reformat_code.sh
+++ reformat_code.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # Go to the directory where the script is located
 cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd
-black --line-length 79 --exclude photogrammetry_importer/ext photogrammetry_importer
-black --line-length 79 example_view_synthesis_scripts
-black --line-length 79 doc/sphinx/source/conf.py
+REQUIRED_VERSION="24.3.0"
+black --required-version ${REQUIRED_VERSION} --line-length 79 --exclude photogrammetry_importer/ext photogrammetry_importer
+black --required-version ${REQUIRED_VERSION} --line-length 79 example_view_synthesis_scripts
+black --required-version ${REQUIRED_VERSION} --line-length 79 doc/sphinx/source/conf.py
